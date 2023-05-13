1d623594d701c6ff1440df96cd68cdfb895ae073
Banurr <yermagambetovnurbol@gmail.com>
2023-05-13 20:19:20 +0600

    start of models and dir

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.IntegerField()
