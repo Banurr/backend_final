70d7bb7927b655a07434ca1cb6e9697cbc8d6b1b
Banurr <yermagambetovnurbol@gmail.com>
2023-05-13 20:23:03 +0600

    first view

--- backend_final/urls.py
+++ backend_final/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import shop
+from shop import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', shop.views.main)
 ]
