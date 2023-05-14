8e39e3c20dc405bec94a4c172d68182c6e1e9619
Banurr <117210664+Banurr@users.noreply.github.com>
2023-05-14 23:56:21 +0600

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -15,20 +15,15 @@ def current_category(request, idx=1):
 def allofthem(request):
     user = request.user
     products = Product.objects.all()
-<<<<<<< HEAD
     categories = Category.objects.all()
     content = {"all": products, "user": user, "categories": categories}
     return render(request, 'allproducts.html', content)
 
-=======
     content = {"all": products, "user": user}
-<<<<<<< HEAD
     return render(request, 'allproducts.html', content)
 
-=======
     return render(request,'allproducts.html',content)
->>>>>>> origin/main
->>>>>>> 9830b4f8d5a969755270e04ece85d31c6bb6807d
+
 
 # Create your views here.
 def upload_media(request):
@@ -59,30 +54,24 @@ def main(request):
 def about_us(request):
     categories = Category.objects.all()
     user = request.user
-<<<<<<< HEAD
+
     return render(request, 'about_us.html', {"user": user})
 
 
 def current_product(request, idx):
-=======
-<<<<<<< HEAD
+
     return render(request, 'about_us.html', {"user": user, "categories": categories})
 
 
 def current_product(request, idx):
     categories = Category.objects.all()
->>>>>>> 9830b4f8d5a969755270e04ece85d31c6bb6807d
     user = request.user
     product = get_object_or_404(Product, id=idx)
     comment = Comment.objects.filter(product=product)
     print(product)
-<<<<<<< HEAD
     content = {"product": product, "comment": comment, "user": user}
     return render(request, 'current_product.html', content)
-=======
     content = {"product": product, "comment": comment, "user": user, "categories": categories}
     return render(request, 'current_product.html', content)
-=======
     return render(request, 'about_us.html', {"user":user})
->>>>>>> origin/main
->>>>>>> 9830b4f8d5a969755270e04ece85d31c6bb6807d
+
